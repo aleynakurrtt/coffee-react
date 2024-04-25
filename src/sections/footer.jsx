041926e10d558e91e9ee9c728071cebab79b9987
Aleyna <153866970+aleynakurrtt@users.noreply.github.com>
2024-04-26 02:16:12 +0300

    Update footer.jsx

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../style/style.scss'
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <footer class=" foot bg-dark text-white py-4">
+                <div class="container foot px-5">
+                    <div class="text-center ">
+                        ALEYNA KURT 2024
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
 

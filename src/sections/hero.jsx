37d9688d236ea3f61a0079b7103e7e6dddd8cf40
Aleyna <153866970+aleynakurrtt@users.noreply.github.com>
2024-04-26 02:15:54 +0300

    Update hero.jsx

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
             <div className="container hero text-center">
                 <h1 className='welcome'>HOŞGELDİNİZ</h1>
                 <p className='quality'>Taze ve kaliteli kahve çekirdeklerini bulabileceğiniz <br /> Türkiyenin en büyük online kahve mağazası.</p>
-                <button type="button" class="btn firstButton ">Primary</button>
+                <button type="button" class="btn firstButton ">Keşfet</button>
             </div>
         </>
     )
